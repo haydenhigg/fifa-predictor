c1b6ced845c778d7c7e4f2c22bd670136847f012
Hayden Higginbotham <hayden@higginbotham.io>
2026-06-27 00:46:47 -0400

    Use multi-head classifier in predict.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module fifa-predictor
 
 go 1.25.5
+
+require github.com/haydenhigg/lynn v1.10.0 // indirect
--- predict.go
+++ predict.go
@@ -3,24 +3,25 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"github.com/haydenhigg/lynn"
 	"io"
 	"math"
 	"os"
-	"slices"
 	"strconv"
 	"time"
 )
 
 const INPUT_PATH = "fifa_matches.csv"
-const LATENT_LEARNING_RATE = 0.05
-const TEST_PORTION = 0.1
+
+const LEARNING_RATE = 0.1
+const TEST_FRACTION = 0.05
 
 type Match struct {
 	Date    time.Time
 	Status  string
 	Teams   []string
 	Scores  []int
-	Outcome float32
+	Outcome []float64
 }
 
 func readMatches(fileName string) ([]Match, error) {
@@ -44,11 +45,13 @@ func readMatches(fileName string) ([]Match, error) {
 		homeScore, _ := strconv.Atoi(record[3])
 		awayScore, _ := strconv.Atoi(record[5])
 
-		outcome := float32(0.5)
+		outcome := make([]float64, 3)
 		if homeScore > awayScore {
-			outcome = 1
+			outcome[0] = 1
+		} else if awayScore > homeScore {
+			outcome[2] = 1
 		} else {
-			outcome = 0
+			outcome[1] = 1
 		}
 
 		matches = append(matches, Match{
@@ -67,14 +70,50 @@ type Latent struct {
 	Offense, Defense float64
 }
 
+func makeXs(latents map[string]*Latent, teams []string) []float64 {
+	xs := make([]float64, 2*len(teams))
+
+	for i, team := range teams {
+		xs[2*i] = latents[team].Offense
+		xs[2*i+1] = latents[team].Defense
+	}
+
+	return xs
+}
+
+func errors(ps, target []float64) []float64 {
+	errors := make([]float64, len(ps))
+
+	for i, p := range ps {
+		errors[i] = target[i] - p
+	}
+
+	return errors
+}
+
+func argmax(xs []float64) int {
+	arg := 0
+
+	for i, x := range xs {
+		if x > xs[arg] {
+			arg = i
+		}
+	}
+
+	return arg
+}
+
 func main() {
 	matches, err := readMatches(INPUT_PATH)
 	if err != nil {
 		panic(err)
 	}
-	// numTestMatches := int(float64(len(matches)) * TEST_PORTION)
 
 	latents := map[string]*Latent{}
+	model := lynn.NewLinearGroup(3, 4)
+
+	numTestMatches := int(float64(len(matches)) * TEST_FRACTION)
+	numCorrect := 0
 
 	for i := len(matches) - 1; i >= 0; i-- {
 		match := matches[i]
@@ -92,32 +131,41 @@ func main() {
 			logit := latents[homeTeam].Offense - latents[awayTeam].Defense
 			gradient := float64(match.Scores[home]) - math.Exp(logit)
 
-			step := gradient * LATENT_LEARNING_RATE
+			step := gradient * LEARNING_RATE
 			latents[homeTeam].Offense += step
 			latents[awayTeam].Defense -= step
 		}
-	}
 
-	teams := make([]string, len(latents))
-	i := 0
-	for team := range latents {
-		teams[i] = team
-		i++
-	}
+		// update model
+		xs := makeXs(latents, match.Teams)
 
-	slices.SortFunc(teams, func(a, b string) int {
-		aTotal := latents[a].Offense + latents[a].Defense
-		bTotal := latents[b].Offense + latents[b].Defense
+		prediction := lynn.Softmax(model.Feed(xs))
+		gradient := errors(prediction, match.Outcome)
+		model.Step(xs, gradient, LEARNING_RATE)
 
-		if aTotal < bTotal {
-			return 1
+		fmt.Println(match.Outcome, prediction)
+
+		// record model metrics
+		if i < numTestMatches {
+			if argmax(prediction) == argmax(match.Outcome) {
+				numCorrect++
+			}
 		}
-
-		return -1
-	})
-
-	for i := range 10 {
-		latent := latents[teams[i]]
-		fmt.Println(teams[i], latent.Offense+latent.Defense, latent)
 	}
+
+	// print model metrics
+	fmt.Printf("\n%.1f%% correct\n", 100*float64(numCorrect)/float64(numTestMatches))
+	fmt.Printf("%.3f log loss\n", 0.)
+
+	// print hypothetical match prediction
+	fmt.Println("\nHYPOTHETICAL MATCH:")
+	hypo := []string{"Colombia", "Portugal"}
+	hypoPrediction := lynn.Softmax(model.Feed(makeXs(latents, hypo)))
+
+	fmt.Printf(
+		"%s\t%.0f%%\n%s\t%.0f%%\nDraw\t\t%.0f%%\n",
+		hypo[0], 100*hypoPrediction[0],
+		hypo[1], 100*hypoPrediction[2],
+		100*hypoPrediction[1],
+	)
 }
